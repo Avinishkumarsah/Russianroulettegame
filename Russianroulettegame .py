e244ec13ea52a9ac075f8f7dd6d631e1c29712c0
Avinishkumarsah <avinishksah@gmail.com>
2023-11-26 22:17:09 +0545

    Delete Russianroulettegame .py

--- Russianroulettegame .py
+++
@@ -1,8 +0,0 @@
-import random
-num = random.randint(1,10)
-guess = input("Guess a number between 1 to 10")
-guess = int(guess)
-if guess == num:
-    print("You Won!")
-else:
-    print("You lose!")
