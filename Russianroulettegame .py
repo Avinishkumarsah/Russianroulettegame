8cca9392a82938376d3636ac1351ea512d4fdad3
Avinishkumarsah <avinishksah@gmail.com>
2023-11-26 22:25:18 +0545

    Russianroulettegame .py

--- Russianroulettegame .py
+++ Russianroulettegame .py
@@ -1,6 +1,6 @@
 import random
-num = random.randint(1,10)
-guess = input("Guess a number between 1 to 10")
+num = random.randint(1,100)
+guess = input("Guess a number between 1 to 100")
 guess = int(guess)
 if guess == num:
     print("You Won!")
